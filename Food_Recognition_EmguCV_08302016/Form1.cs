18107bb3d421b1676f56aa05d0f9b7ec5b7d9b50
Anish <Anish@YAADADA>
2016-08-31 16:01:24 -0500

    Add project files.

---
+++ Food_Recognition_EmguCV_08302016/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+
+namespace Food_Recognition_EmguCV_08302016
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
